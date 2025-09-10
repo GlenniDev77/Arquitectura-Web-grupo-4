13524b35772c80cb07be0c7e194739b958db2b50
Jorge Glenni <jorgeglenni8@gmail.com>
2025-09-10 13:35:15 -0500

    Ahora si funciona Postman

--- Proyecto-Arqui-Web-Jorge/src/main/java/pe/edu/upc/proyectoarquiwebjorge/controllers/RolController.java
+++ Proyecto-Arqui-Web-Jorge/src/main/java/pe/edu/upc/proyectoarquiwebjorge/controllers/RolController.java
@@ -2,9 +2,7 @@ package pe.edu.upc.proyectoarquiwebjorge.controllers;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.proyectoarquiwebjorge.dtos.RolDTO;
 import pe.edu.upc.proyectoarquiwebjorge.entities.Rol;
 import pe.edu.upc.proyectoarquiwebjorge.servicesinterfaces.IRolService;
@@ -14,19 +12,24 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/roles")
-
 public class RolController {
-/*
+
     @Autowired
     private IRolService rS;
 
     @GetMapping
     public List<RolDTO> list() {
-        return (List)this.rS.list().stream().map(y -> {
+        return this.rS.list().stream().map(y -> {
             ModelMapper mapper = new ModelMapper();
             return (RolDTO)mapper.map(y, RolDTO.class);
         }).collect(Collectors.toList());
     }
+    @PostMapping
+    public void insert(@RequestBody RolDTO dto) {
+        ModelMapper mapper = new ModelMapper();
+        Rol d=mapper.map(dto,Rol.class);
+        rS.insert(d);
+    }
 
- */
+
 }
