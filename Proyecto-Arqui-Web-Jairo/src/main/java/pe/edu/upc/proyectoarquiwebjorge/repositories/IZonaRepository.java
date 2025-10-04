c19cc2b4daaff749c07196da3da452c9241da1e8
Jairo <latamroleplayrp@gmail.com>
2025-10-04 18:43:19 -0500

    QUITE MI ANTERIOR PROYECTO PARA NO GENERAR PROBLEMAS EN EL RENDER

--- Proyecto-Arqui-Web-Jairo/src/main/java/pe/edu/upc/proyectoarquiwebjorge/repositories/IZonaRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.edu.upc.proyectoarquiwebjorge.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.edu.upc.proyectoarquiwebjorge.entities.Zona;
-
-@Repository
-public interface IZonaRepository extends JpaRepository<Zona,Integer> {
-}
--- Proyecto-Arqui-Web-Jairo/src/main/java/pe/edu/upc/proyectoarquiwebjorge/servicesinterfaces/IZonaService.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.proyectoarquiwebjorge.servicesinterfaces;
-import pe.edu.upc.proyectoarquiwebjorge.entities.Zona;
-import java.util.List;
-
-public interface IZonaService {
-    public List<Zona> list();
-    public void insert(Zona zona);
-}
