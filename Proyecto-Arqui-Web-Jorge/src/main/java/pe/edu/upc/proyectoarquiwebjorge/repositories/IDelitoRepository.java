b9aa1382eb4ab9d6c61ae0406b440e03b0b6fffe
Tiago-RT <rodrigotiagotm2006@gmail.com>
2025-11-04 18:54:42 -0500

    Delete Proyecto-Arqui-Web-Jorge directory

--- Proyecto-Arqui-Web-Jorge/src/main/java/pe/edu/upc/proyectoarquiwebjorge/repositories/IDelitoRepository.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.proyectoarquiwebjorge.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import pe.edu.upc.proyectoarquiwebjorge.entities.Delito;
-
-public interface IDelitoRepository extends JpaRepository<Delito, Integer> {
-
-}
